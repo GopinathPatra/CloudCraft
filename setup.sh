5b43beba18c7b80f0ff1f2fb6eeeeea36d86ccd5
GopinathPatra <gopinathpatra332@gmail.com>
2026-07-05 08:36:17 +0000

    Day 2: Add idempotent setup automation script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,77 @@
 #!/bin/bash
 
-echo "Hello CloudCraft"
+set -e
+GREEN="\e[32m"
+RED="\e[31m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+RESET="\e[0m"
 
-echo "Welcome to Linux"
+info() {
+    echo -e "${BLUE}[INFO]${RESET} $1"
+}
 
-echo "Today I learned chmod."
+success() {
+    echo -e "${GREEN}[SUCCESS]${RESET} $1"
+}
+
+warning() {
+    echo -e "${YELLOW}[WARNING]${RESET} $1"
+}
+
+error() {
+    echo -e "${RED}[ERROR]${RESET} $1"
+}
+
+echo "======================================"
+echo "   CloudCraft Setup Script"
+echo "======================================"
+echo "Welcome, $USER"
+echo "Today's Date: $(date)"
+echo "Current Directory: $(pwd)"
+
+
+install_package() {
+    local package=$1
+
+    info "Checking $package..."
+
+    if command -v "$package" >/dev/null 2>&1
+    then
+        success "✅ $package is already installed."
+    else
+        info "Installing $package..."
+        sudo apt install -y "$package"
+
+        if command -v "$package" >/dev/null 2>&1
+        then
+            success "✅ $package installed successfully."
+        else
+          error "❌ Failed to install $package."
+          exit 1
+    fi
+fi
+echo
+}
+info "Updating package list..."
+
+sudo apt update
+
+success "Package list updated."
+
+echo
+PACKAGES=(
+    git
+    curl
+    wget
+    vim
+    tree
+    unzip
+)
+for pkg in "${PACKAGES[@]}"
+do
+    install_package "$pkg"
+done
+echo "========================================="
+success "CloudCraft setup completed successfully!"
+echo "========================================="
